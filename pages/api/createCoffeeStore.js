7b03e736478ef755ed44c3034ae204c3f8bdbfa3
Moustafa Ahmed <moustafa@Moustafa-Laptop.localdomain>
2022-07-07 11:44:56 +0200

    Refactored the createCoffeeStore Api Modified the fetch to have body params from the request body instead of hardcoding the values

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,15 +1,47 @@
-const Airtable = require("airtable");
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(
-	process.env.AIRTABLE_BASE_KEY
-);
-const table = base("coffee-stores");
+import {getMinifiedRecords, table} from "../../lib/airtable";
 
-const createCoffeeStore = (req, res) => {
-    if (req.method === "POST") {
-        res.json({message: "Airtable"})
-    }else{
-        res.json({message: "Not a post request"})
+const createCoffeeStore = async (req, res) => {
+	if (req.method === "POST" && req.body.id) {
+		const {id, name, address, neighbourhood, voting, imgUrl} = req.body;
+		try {
+			const findCoffeeStoreRecords = await table
+				.select({
+					filterByFormula: `id=${id}`,
+				})
+				.firstPage();
 
-    }
-}
+			if (findCoffeeStoreRecords.length > 0) {
+				const records = getMinifiedRecords(findCoffeeStoreRecords);
+				res.status(200);
+				res.json(records[0]);
+			} else if (id) {
+				const createRecords = await table.create([
+					{
+						fields: {
+							id,
+							name,
+							address,
+							neighbourhood,
+							voting,
+							imgUrl,
+						},
+					},
+				]);
+				const records = getMinifiedRecords(createRecords);
+
+				res.json({message: "Create a record", records});
+			} else {
+				res.status(400);
+				res.json({message: "Id or name is missing"});
+			}
+		} catch (error) {
+			console.error("Error finding store: ", error);
+			res.status(500);
+			res.json({message: "Error finding store", error});
+		}
+	} else {
+		res.status(400);
+		res.json({message: "Id is missing"});
+	}
+};
 export default createCoffeeStore;
